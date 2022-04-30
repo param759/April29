04ecdc8d143a76c1b54d216a2d3f79350a6a7593
param singh <param759@gmail.com>
2022-04-29 22:30:57 -0400

    second commit

--- src/test/java/April29/AMy1/HomeTest.java
+++ src/test/java/April29/AMy1/HomeTest.java
@@ -1,9 +1,11 @@
 package April29.AMy1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class HomeTest {
-
+public WebDriver driver;
 	@Test
 	public void goal()
 	{
@@ -13,6 +15,8 @@ public class HomeTest {
 	@Test
 	public void goal2()
 	{
-		System.out.println("work not done");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\15199\\Desktop\\latestchrome\\chromedriver.exe");
+		 driver=new ChromeDriver();	
+		 driver.get("https://www.salesforce.com/ca/?ir=1");
 	}
 }
