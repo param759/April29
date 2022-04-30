539f0c8af6f466775f987c0e0adfcf93b696b63c
param singh <param759@gmail.com>
2022-04-29 22:18:28 -0400

    first commit

---
+++ src/test/java/April29/AMy1/HomeTest.java
@@ -0,0 +1,18 @@
+package April29.AMy1;
+
+import org.testng.annotations.Test;
+
+public class HomeTest {
+
+	@Test
+	public void goal()
+	{
+		System.out.println("workto be done");
+	}
+	
+	@Test
+	public void goal2()
+	{
+		System.out.println("work not done");
+	}
+}
